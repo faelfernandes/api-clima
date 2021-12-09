774f6206aaf28690f73c9d5795d08fd8361ee665
faelfernandes <ctt.rafaelfernandes@gmail.com>
2021-12-09 14:09:40 -0300

    Alterando colunas para double

---
+++ scr/database/migrations/2021_12_08_195306_create_climates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClimatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('climates', function (Blueprint $table) {
+            $table->id();
+            $table->string('city_name');
+            $table->string('state_code');
+            $table->string('country_code');
+            $table->double('temp');
+            $table->double('temp_min');
+            $table->double('temp_max');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('climates');
+    }
+}
