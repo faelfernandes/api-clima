f1466491395b9674686b1623b43dad0da59af893
faelfernandes <ctt.rafaelfernandes@gmail.com>
2021-12-10 17:09:27 -0300

    Adicionando filtro na model

--- scr/app/Models/Climate.php
+++ scr/app/Models/Climate.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Climate extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'city_name',
+        'country_code',
+        'temp',
+        'temp_min',
+        'temp_max'
+    ];
 }
