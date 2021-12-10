05de84bc52282b4c079d3360ba588e37f2be21cf
faelfernandes <ctt.rafaelfernandes@gmail.com>
2021-12-10 17:18:53 -0300

    Alterações na Classe da API OpenWeather

--- scr/app/Http/Controllers/OpenWeatherMap.php
+++ scr/app/Http/Controllers/OpenWeatherMap.php
@@ -8,19 +8,19 @@ use Illuminate\Support\Facades\Http;
 class OpenWeatherMap extends Controller
 {
     /**
-     * URL Base das APIs
+     * API Base URL
      * @var string
      */
     const BASE_URL = 'https://api.openweathermap.org';
 
     /**
-     * Chave de acesso da API
+     * API access key
      * @var string
      */
     private $apiKey;
 
     /**
-     * Método responsável por construir a classe definindo a chave de API
+     * Method responsible for constructing the class by setting the API key
      * @param string $apiKey
      */
     public function __construct()
@@ -29,64 +29,41 @@ class OpenWeatherMap extends Controller
     }
 
     /**
-     * Método responsável por obter o clima atual de uma cidade no Brasil
+     * Method responsible for obtaining the current climate of a city in Brazil
      * @param string $city
-     * @param string $state
      * @return array
      */
-    public function getCityTemperatureInformation($state, $city)
+    public function getCityTemperatureInformation($city)
     {
         $city = $this->getEndpoint('/data/2.5/weather', [
-            'q' => $city . ',BR-' . $state . ',BRA'
+            'q' => $city
         ]);
 
-        if ($this->getStatus($city['cod'])) {
-            return $city->body();
+        if ($city['cod'] !== 200) {
+            return $city->json();
         }
 
         return [
-            "data" => [
-                "city_name"     => $city['name'],
-                "state_code"    => $state,
-                "country_code"  => $city['sys']['country'],
-                "temp"          => $city['main']['temp'],
-                "temp_min"      => $city['main']['temp_min'],
-                "temp_max"      => $city['main']['temp_max'],
-                "cod"           => 200
-            ],
+            "city_name"     => $city['name'],
+            "country_code"  => $city['sys']['country'],
+            "temp"          => $city['main']['temp'],
+            "temp_min"      => $city['main']['temp_min'],
+            "temp_max"      => $city['main']['temp_max'],
+            "cod"           => 200
         ];
     }
 
     /**
-     * Método responsável por executar a culstar GET na API do Open Wather Map
+     * Method responsible for executing the Culstar GET in the Open Wather Map API
      * @param string $resource
      * @param array $params
      * @return array
      */
     private function getEndpoint($resource, $params = [])
     {
-        // PARÂMETROS ADICIONAIS
         $params['units'] = 'metric';
-        //$params['lang'] = 'pt_br';
         $params['appid'] = $this->apiKey;
-
-        //ENDPOINT
         $endpoint = self::BASE_URL . $resource . '?' . http_build_query($params);
-        $response = Http::get($endpoint);
-
-        return $response;
-    }
-
-    /**
-     * Método responsável por verificar se o status do retorno da API é diferente de 200
-     * @param string $status
-     * @return bool
-     */
-    public function getStatus($status)
-    {
-        if ($status !== 200) {
-            return true;
-        }
-        return false;
+        return Http::get($endpoint);
     }
 }
