736e7cebb8125f80732797250aa90f065e7d75d7
faelfernandes <ctt.rafaelfernandes@gmail.com>
2021-12-10 17:07:40 -0300

    Atualizando rotas da api

--- scr/routes/api.php
+++ scr/routes/api.php
@@ -4,35 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClimateController;
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// List climates
 Route::get('climates', [ClimateController::class, 'index']);
-
-// List single climate
-Route::get('climate/{id}', [ClimateController::class, 'show']);
-
-// Create new climate
+Route::get('climate/{city}', [ClimateController::class, 'show']);
 Route::post('climate', [ClimateController::class, 'store']);
-
-// Update climate
 Route::put('climate/{id}', [ClimateController::class, 'update']);
-
-// Delete climate
 Route::delete('climate/{id}', [ClimateController::class, 'destroy']);
-
-Route::get('temperature/{state}/{city}', [ClimateController::class, 'teste']);
